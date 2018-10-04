df4fd5fa667c0cc7d58724a0f8ebd1f86decdc03
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-04 13:01:07 -0600

    changes

---
+++ index.js
@@ -0,0 +1,27 @@
+function simpleOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB;
+  let mutliply = numberA * numberB;
+  let divide = numberA / numberB;
+  console.log(adding);
+  console.log(subtract);
+  console.log(mutliply);
+  console.log(divide);
+}
+
+//Skipcount by 2 for 10 times starting at 0
+function skipCounting (2, 10, 0){
+  let number = 0 - 2;
+  for (let i=1; i<=times; i++){
+    number += skipCount;
+    console.log(number);
+  }
+}
+
+//simpleComparison using WHILE and Skip Counting Algorithm
+function howManyTimes (0, 20, 2) {
+  while (0 < 20) {
+    console.log(numberStart += skipCount); // Used to be 2 lines ... console.log(numberStart); numberStart += skipCount;
+    // What is happening with the single line compared to the double line of code?
+  }
+}
