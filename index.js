c8e6c43a85d35b35781668d39a555e39d01f6983
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-04 13:16:08 -0600

    changes

--- index.js
+++ index.js
@@ -30,13 +30,3 @@ simpleOperators (number[0], number[1]);
 simpleOperators (number[2], number[3]);
 skipCounting (2, 10, 11);
 skipCounting (2, 10, 10);
-
-
-let number = [10, 31]; //Boundary Numbers
-let skipCount = 2; //Number of skipCounting between each element of the sequence, used in FOR Loop
-let difference = number[1] - number[0]; //Difference, mandatory expectation
-let startingNumber = number[1]; //Hint: used in the FOR Loop
-for (let i=0; i<=difference; i++){
-  even = (startingNumber += skipCount) % 2;
-  console.log("This number is", even, "; This iteration is number", i+skipCount); //Example of Concatenating
-}
