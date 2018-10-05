0c68388958b173143524feca4820d913590c02db
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-05 13:37:55 -0600

    changes

--- index.js
+++ index.js
@@ -43,3 +43,12 @@ evenNumbers = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
 //printing out entire onject]
 console.log(oddNumbers);
 console.log(evenNumbers);
+
+let oddNumbers = [11, 29]; //Boundary Numbers
+let skipCount = 1; //Number of skipCounting between each element of the sequence, used in FOR Loop
+let difference = number[1] - number[0]; //Difference, mandatory expectation
+let startingNumber = number[0]; //Hint: used in the FOR Loop
+for (let i=0; i<=difference; i++){
+  even = (startingNumber += skipCount) % 2;
+  console.log("This number is", even, "; This iteration is number", i+skipCount); //Example of Concatenating
+}
