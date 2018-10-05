67c7ac1fc7c933fd6400128983da1f4f6c9bfe8b
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-05 13:54:18 -0600

    changes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ evenNumbers = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
 console.log(oddNumbers);
 console.log(evenNumbers);
 
-let number = [10, 29]; //Boundary Numbers
+let Numbers = [10, 29]; //Boundary Numbers
 let skipCount = 2; //Number of skipCounting between each element of the sequence, used in FOR Loop
 let difference = number[2] - number[0]; //Difference, mandatory expectation
 let startingNumber = number[10]; //Hint: used in the FOR Loop
