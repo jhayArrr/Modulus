935062329128d417bcc03edf61488ec81b4235f7
jhayArrr <43151443+jhayArrr@users.noreply.github.com>
2018-10-05 13:42:54 -0600

    changes

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ console.log(evenNumbers);
 
 let Array = [10, 29]; //Boundary Numbers
 let skipCount = 2; //Number of skipCounting between each element of the sequence, used in FOR Loop
-let difference = number[2] - number[0]; //Difference, mandatory expectation
+let difference = number[1] - number[0]; //Difference, mandatory expectation
 let startingNumber = number[10]; //Hint: used in the FOR Loop
-for (let i=1; i<=difference; i++){
+for (let i=0; i<=difference; i++){
   even = (startingNumber += skipCount) % 2;
   console.log("This number is", even, "; This iteration is number", i+skipCount); //Example of Concatenating
 }
